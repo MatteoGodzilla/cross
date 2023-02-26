0cfb24c845e5923fa861dd44cd7a4e62203c1f93
DallaTheMage <124520719+DallaTheMage@users.noreply.github.com>
2023-02-26 20:17:41 +0100

    Update customs_id.py

--- routes/customs_id.py
+++ routes/customs_id.py
@@ -17,9 +17,9 @@ def GetCustom(id:int):
         # Convert to HTTPException
         return Response("There was an error with connecting to the database (500)",500)
 
-    #InitializeIfNeeded(conn) - At the moment this function call is useless.
+    # InitializeIfNeeded(conn) - At the moment this function call is useless.
     cursor = conn.cursor()
-    param_query = "SELECT * FROM customs WHERE id = ? AND visible = 1"
+    param_query = "SELECT * FROM customs WHERE id = ? AND visible = 1;"
     # data parameter has to be either a tuple or a list
     cursor.execute(param_query, [id])
     res=cursor.fetchone()
@@ -36,13 +36,41 @@ def GetCustom(id:int):
 # Attempts to change a custom already in the database
 # Request must have an Authorization code attached to the header
 @customs_id.patch("/customs/{id}")
-def PatchCustom(id:int,authorization:str|None=Header(default=None)):
+def PatchCustom(id:int,authorization:str, elem:Custom|None=Header(default=None)):
     if CheckAuth(authorization):
-        return Response("Welcome!")
+        conn = CreateConnection()
+        if conn is None:
+            #Convert to HTTPException
+            return Response("There was an error with connecting to the database (500)",500)
+        cursor = conn.cursor()
+        
+        param_query = "SELECT * FROM customs WHERE id = ?"
+        cursor.execute(param_query, [id])
+        old = cursor.fetchone()
+       
+        new_data = json.loads(elem.json())
+        keys = new_data.keys()
+        values = new_data.values()
+        param_query = "UPDATE customs SET "
+        for e in keys:
+            param_query += e+"= ?, "
+        param_query += ";"
+        cursor.execute(param_query, values)
+        
+        param_query = "SELECT * FROM customs WHERE id = ?"
+        cursor.execute(param_query, [id])
+        new = cursor.fetchone()
+        
+        cursor.close()
+        DestroyConnection(conn)
+        if old == new:
+            return Response("An error as occured (500)", 500)
+        else:
+            return Response("Resources updated successfully (200)", 200)
     else:
         # Convert to raise HTTPException
         return Response("You have to login first",401)
-
+    
 # DELETE /api/v1/customs/<id>
 # Attempts to delete a custom already in the database
 # Request must have an Authorization code attached to the header
@@ -54,11 +82,11 @@ def DeleteCustom(id:int,authorization:str|None=Header(default=None)):
             #Convert to HTTPException
             return Response("There was an error with connecting to the database (500)",500)
         cursor = conn.cursor()
-        param_query = "DELETE * FROM customs WHERE id = ?"
+        param_query = "DELETE * FROM customs WHERE id = ?;"
         cursor.execute(param_query, [id])
         cursor.close()
         
-        #CheckExistence(conn, id) - Probably useful to check the deletion and the update of one custom
+        # CheckExistence(conn, id) - Probably useful to check the deletion and the update of one custom
         if CheckExistence(conn, id) == 0:
             DestroyConnection(conn)
             return Response("Resource deleted successfully (204)", 204)
