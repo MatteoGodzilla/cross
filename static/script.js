a0f06a05816d00cb9b90c06f8523966d04cd20d1
DallaTheMage <lorenzo.dallara1@gmail.com>
2023-04-05 22:29:19 +0200

    Added JSON => HTML function for Megamixes

--- static/script.js
+++ static/script.js
@@ -41,3 +41,57 @@ function CustomToPage(custom,id){
 
     return div
 }
+
+
+
+function MGMX_JsonToTable(json)
+{
+	// From JSON to Javascript object
+	var myObj = JSON.parse(json);
+	
+	// Setting up the data structures to build the table section 
+	const table = document.createElement("table");
+	
+	// keys => simply the description (wrote in left column) of the content in the right column
+	var keys = ["Megamix name:", "Download link:", "Video preview:", "Customs:"]
+	
+	// values => the data content of the JSON that we are going to write in the right column
+	var values = [myObj.Name, myObj.DownloadLink, myObj.VideoPreview];
+	
+	// Creating the table structure
+	for(let c = 0; c < 4; c++)
+	{
+		const row = document.createElement("tr");
+		const frstCol = document.createElement("td");
+		const sndCool = document.createElement("td");
+		
+		if(c < 3)
+		{
+			frstCol.appendChild(document.createTextNode(keys[c]));
+			row.appendChild(frstCol);
+			
+			sndCol.appendChild(document.createTextNode(values[c]));
+			row.appendChild(sndCol);
+		}
+		else
+		{
+			frstCol.appendChild(document.createTextNode(keys[c]));
+			row.appendChild(frstCol);
+			
+			const customs = document.createElement("ul");
+			for(x in myObj.Customs)
+			{
+				const ls = document.createElement("li");
+				ls.appendChild(document.createTextNode(x))
+				customs.appendChild()
+			}
+			sndCol.appendChild(customs);
+			row.appendChild(sndCol);
+		}
+		table.appendChild(row);
+		
+	}
+	
+	//In the end, append the table
+	document.getElementById("show").appendChild(table);
+}
