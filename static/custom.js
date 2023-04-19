3236f69bfbf7318c396d203603f1c638a7f82ede
MatteoGodzilla <catena.matteo90@gmail.com>
2023-04-19 23:35:26 +0200

    restructured html pages

--- static/custom.js
+++ static/custom.js
@@ -1,37 +1,25 @@
 function AddCustom(){
-    
+	alert("ADD")
 }
 
 function EditCustom(){
-
+	alert("EDIT")
 }
-
-function AddMegamix(){
-
-}
-
-function EditMegamix(){
-
-}
-
-let count = 10;
 
 async function GetLatest(){
-    latestCustoms = document.querySelector("#latestCustoms")
+    latestCustoms = document.querySelector("#show")
     latestCustoms.innerHTML = ""
 
-    ids = await fetch(`/api/v1/custom/latest/${count}`).then(res => res.json())
+    ids = await fetch(`/api/v1/custom/latest/`).then(res => res.json())
     console.log(ids)
     for(id of ids){
         json = await fetch(`/api/v1/custom/${id}`).then(res => res.json());
         latestCustoms.appendChild(CustomToPage(json,id))
     }
-
-    count += 10
 }
 
 function CustomToPage(custom,id){
-    console.log(custom)
+    //console.log(custom)
     div = document.createElement("div")
     str = `${id}: ${custom.IDTag} / `
     for(song of custom.Songs){
@@ -46,21 +34,21 @@ function CSTM_JSONToTable(json)
 {
 	// From JSON to Javascript object
 	var myObj = JSON.parse(json);
-	
+
 	// Initializing the row that will contain the JSON object
 	const row = document.createElement("tr");
 
 	// keys => simply the description of the content in the right column (PROBABLY DEPRECATED)
 	//var keys = ["BPM:", "DownloadLink:", "Songs:", "Charter:", "Mixer:", "Difficulties:", "Charts:", "DeckSpeeds:", "VideoLink:", "Notes:"];
-	
+
 	// values => the data content of the JSON that we are going to write in the right column
 	var values = [myObj.BPM, myObj.DownloadLink, myObj.Songs, myObj.Charter, myObj.Mixer, myObj.Difficulties, myObj.Charts, myObj.DeckSpeeds, myObj.VideoLink, myObj.Notes];
-	
+
 	// Creating the table structure
 	for(let c = 0; c < 10; c++)
 	{
 		const col = document.createElement("td");
-		
+
 		if(c == 2 || (c >= 5 && c <= 7))
 		{
 			const customs = document.createElement("ul");
@@ -81,47 +69,5 @@ function CSTM_JSONToTable(json)
 	}
 
 	//In the end, append the table
-	document.getElementById("show").appendChild(row);
-}
-
-function MGMX_JSONToTable(json)
-{
-	// From JSON to Javascript object
-	var myObj = JSON.parse(json);
-
-	// Initializing the row that will contain the JSON object
-	const row = document.createElement("tr");
-
-	// keys => simply the description (wrote in left column) of the content in the right column (PROBABLY DEPRECATED)
-	//var keys = ["Megamix name:", "Download link:", "Video preview:", "Customs:"] 
-	
-	// values => the data content of the JSON that we are going to write in the right column
-	var values = [myObj.Name, myObj.DownloadLink, myObj.VideoPreview];
-	
-	// Creating the table structure
-	for(let c = 0; c < 4; c++)
-	{
-		const col = document.createElement("td");
-		
-		if(c < 3)
-		{
-			col.appendChild(document.createTextNode(values[c]));
-			row.appendChild(col);
-		}
-		else
-		{
-			const customs = document.createElement("ul");
-			for(x in myObj.Customs)
-			{
-				const ls = document.createElement("li");
-				ls.appendChild(document.createTextNode(x));
-				customs.appendChild(ls);
-			}
-			col.appendChild(customs);
-			row.appendChild(col);
-		}		
-	}
-
-	//In the end, append the row
 	document.getElementById("show").appendChild(row);
 }
